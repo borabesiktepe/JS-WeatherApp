b173afe4339226d91eea8ef4af8e00fbe14b0b61
Bora Beşiktepe <borabesiktepe@gmail.com>
2023-02-15 16:46:36 +0300

    Added JS codes

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weatherBox");
+const weatherDetails = document.querySelector(".weatherDetails");
+
+search.addEventListener('click', () => {
+    const APIKey = "ce2f9276234e553abda46b262d77bdc5";
+    const city = document.querySelector(".search-box input").value;
+
+    if (city === "")
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}"`)
+        .then(response => response.json())
+        .then(json => {
+            const temperature = document.querySelector(".weather-box temperature");
+            const description = document.querySelector(".weather-box description");
+            const humidity = document.querySelector(".weather-box humidity");
+            const wind = document.querySelector(".weather-box wind");
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+    })
+})
